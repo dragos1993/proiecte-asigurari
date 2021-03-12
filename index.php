f48458ed508b9a1b21c82119fc9463f7b6177738
dragos1993 <59688724+dragos1993@users.noreply.github.com>
2021-03-12 19:57:11 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
+    <link rel="stylesheet" type="text/css" href="style/style2.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet"> 
+    <title>Proiecte</title>
+</head>
+<body>
+
+    <!--creare bara de navigare-->
+    <div class="header">
+        <div class="container">
+            <div class="logo">
+                <img src="imagini/desenul-tehnic.jpg">                
+             </div>
+        </div>
+    </div>
+        <div class="login">
+            <div class="container">
+            <ul>
+            <h1>Bine ai venit!<br>Alege materia:</h1><br><br><br>
+                     <div class="nav">
+                   <li><a href="OM-selectare.php"> OM2 </a></li><br><br><br>
+                   <li><a href="InformaticaAplicata.php"> Informatica Aplicata </a></li><br><br><br>
+
+                   <h1> Vrei o asigurare ?? --- 0755/422/527 ------ Adina </h1><br>
+                <img src="imagini/asigurari.jpg" alt="Asigurari" style="width:750px;height:400px;">
+<br><br>
+
+           <<!--     <h1 style="float:center;">Printare-3D --- 0726/883/794 ------ Dragos </h1>
+            <h1> </h1>
+                <img src="imagini/3d.jpg" alt="print3dq" style="width:550px;height:400px;"> -->
+             </ul>
+                     </div>
+                </div>
+            </div>
+        </div>
+
+
+</form>
+</body>
+</html>
